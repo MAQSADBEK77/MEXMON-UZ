f1cf884df1790c57ef1c0aae0cb16a973aaa9177
Maqsadbek <110594640+MAQSADBEK77@users.noreply.github.com>
2024-04-25 15:37:28 +0500

    Update summa

--- src/pages/IdentifyOrigin.jsx
+++ src/pages/IdentifyOrigin.jsx
@@ -320,7 +320,7 @@ function IdentifyOrigin() {
                     <label>ИТОГОВАЯ СУММА ТУРСБОРА: </label>
                   </td>
                   <td className="text-info font-bold">
-                    <b>{dataa != null && dataa.HowDays * 34000}</b>
+                    <b>{dataa != null && dataa.HowDays * 34 + " 000, 00"}</b>
                   </td>
                 </tr>
                 <tr>
